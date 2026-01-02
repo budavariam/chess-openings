5eb7e1cf4cb76a58dff65df6a630cf9815c23942
Mátyás Budavári <33528704+budavariam@users.noreply.github.com>
2026-01-02 22:55:27 +0100

    Add custom render functions with ToastSettingsProvider

---
+++ src/test/test-utils.tsx
@@ -0,0 +1,24 @@
+import { renderHook as originalRenderHook, render as originalRender } from '@testing-library/react';
+import { ToastSettingsProvider } from './ToastSettingsProvider';
+import type { ReactNode } from 'react';
+
+const AllProviders = ({ children }: { children: ReactNode }) => (
+  <ToastSettingsProvider>{children}</ToastSettingsProvider>
+);
+
+export const renderHook: typeof originalRenderHook = (hook, options) => {
+  return originalRenderHook(hook, {
+    wrapper: AllProviders,
+    ...options,
+  });
+};
+
+export const render: typeof originalRender = (ui, options) => {
+  return originalRender(ui, {
+    wrapper: AllProviders,
+    ...options,
+  });
+};
+
+// Re-export everything else
+export * from '@testing-library/react';
